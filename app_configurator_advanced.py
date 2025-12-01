a522933d9ecc8a48f1115f81ecbc4ad364d40455
maurofazzari1990-jpg <maurofazzari1990@gmail.com>
2025-12-01 21:50:27 +0100

    Update app_configurator_advanced.py

--- app_configurator_advanced.py
+++ app_configurator_advanced.py
@@ -1,3 +1,11 @@
+import streamlit as st
+import json
+import os
+from collections import OrderedDict
+
+# --- CONFIGURAZIONE E STRUTTURA DATI ---
+# ... (tutto il resto del codice che segue)
+
 # --- 1. IMPOSTAZIONI GENERALI ---
 st.header("1. Impostazioni Generali")
 
